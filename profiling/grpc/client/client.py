a033bb6498f2e11a4a1bd00e1ca6b9ef269627ac
sailesh <saileshwar123@gmail.com>
2022-11-24 19:30:19 +0530

    monitor

--- profiling/grpc/client/client.py
+++ profiling/grpc/client/client.py
@@ -4,14 +4,55 @@ import random
 import grpc
 import mult_pb2
 import mult_pb2_grpc
+import os, resource
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
+
+class MemoryMonitor:
+	def __init__(self):
+		self.keep_measuring = True
+
+	def measure_usage(self):
+		max_mem = 0
+		max_user = 0
+		max_sys = 0
+		while self.keep_measuring:
+			max_mem = max(
+                max_mem,
+                resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            )
+			max_user = max(
+                max_user,
+                resource.getrusage(resource.RUSAGE_SELF).ru_utime
+				#psutil.cpu_percent()
+			)
+			max_sys = max(
+                max_sys,
+                resource.getrusage(resource.RUSAGE_SELF).ru_stime
+            )
+			sleep(0.1)
+			
+		return max_mem, max_user, max_sys
 	
 def run():
 	print("Connecting ...")
 	with grpc.insecure_channel('localhost:50051') as channel:
 		stub = mult_pb2_grpc.CalculatorStub(channel)
 		payload = '*'*1000000
-		response = stub.Multiply(mult_pb2.CalcRequest(Mat1=payload))
-	print("Value received: ", response.value)
+		#response = stub.Multiply(mult_pb2.CalcRequest(Mat1=payload))
+		with ThreadPoolExecutor() as executor:
+			monitor = MemoryMonitor()
+			mem_thread = executor.submit(monitor.measure_usage)
+			try:
+				fn_thread = executor.submit(stub.Multiply, mult_pb2.CalcRequest(Mat1=payload))
+				result = fn_thread.result()
+			finally:
+				monitor.keep_measuring = False
+				max_mem, max_user, max_sys = mem_thread.result()
+				
+			print(f"Peak memory usage: {max_mem}kB")
+			print(f"Peak user time: {max_user}s, Peak system time: {max_sys}s")
+	print("Value received: ", result.value)
 
 
 if __name__ == '__main__':
